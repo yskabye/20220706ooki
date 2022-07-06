9890c8acab1b895674188b03512132ad05f1602e
yskanbye <yassyok@ymail.ne.jp>
2022-07-06 16:55:10 +0900

    first-commit

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Author;
+use App\Http\Requests\AuthorRequest;
+
+class AuthorController extends Controller
+{
+    public function index()
+    {
+        $authors = Author::all();
+        return view('index', ['authors' => $authors]);
+    }
+
+    public function find()
+    {
+        return view('find', ['input' => '']);
+    }
+
+    public function search(Request $request)
+    {
+        $author = Author::find($request->input);
+        $param = [
+            'author' => $author,
+            'input' => $request->input
+        ];
+        return view('find', $param);
+    }
+
+    public function add()
+    {
+        return view('add');
+    }
+
+    public function create(AuthorRequest $request)
+    {
+        $form = $request->all();
+        Author::create($form);
+        return redirect('/');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthorController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [AuthorController::class, 'index']);
+
+Route::get('/find', [AuthorController::class, 'find']);
+Route::post('/find', [AuthorController::class, 'search']);
+
+Route::get('/add', [AuthorController::class, 'add']);
+Route::post('/add', [AuthorController::class, 'create']);
